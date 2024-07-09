a5318bfa6e9b30b8f4ef5027afc199e7fe96300f
Daniel.Martinez@ilumno.com <Daniel.Martinez@ilumno.com>
2024-07-09 00:10:48 -0500

    First route with its view

--- index.js
+++ index.js
@@ -12,7 +12,20 @@ const PRIVATE_APP_ACCESS = '';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const videoGames = 'https://api.hubspot.com/crm/v3/objects/2-32027842?properties=developer,game_genre,name';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(videoGames, { headers });
+        const data = resp.data.results;
+        res.render('Video_Games', { title: 'Video Games | HubSpot APIs', data });
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
